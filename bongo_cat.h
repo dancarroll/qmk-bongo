0a72559f5985dc2de0a076909fb37ab652a14f72
Dan Carroll <dancarroll@gmail.com>
2021-03-04 23:03:23 -0500

    Add initial source drop

---
+++ bongo_cat.h
@@ -0,0 +1,21 @@
+/* Copyright 2021 github.com/dancarroll
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#pragma once
+
+// Renders Bongo Cat animation suitable for QMK-compatible,
+// 128x32 OLED screens.
+void render_bongo_cat(void);
